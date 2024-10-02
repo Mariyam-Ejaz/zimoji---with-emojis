ebbfa7728707044510bdc0d8bd2a37a979943c22
Mariyam-Ejaz <maryam.ejaz@zimogroup.org>
2024-10-03 01:15:21 +0500

    animations

--- src/components/emojis/random-emojis.tsx
+++ src/components/emojis/random-emojis.tsx
@@ -588,7 +588,7 @@ const RandomEmojis = memo(() => {
           clearTimeout(startDelay);
           clearInterval(animationInterval);
         };
-      }, 5000);
+      }, 8000);
 
       return () => clearTimeout(startDelay);
     }
