bf16487cd6a7f6f5c3b33cd323251d1b34bd454a
Mariyam-Ejaz <maryam.ejaz@zimogroup.org>
2024-10-02 10:20:41 +0500

    animations

--- src/components/preloader/content-mapping.tsx
+++ src/components/preloader/content-mapping.tsx
@@ -4,6 +4,7 @@ import { HOLDING_TABS } from "./lib";
 import Welcome from "./welcome";
 import Content from "./content";
 import { AnimatePresence } from "framer-motion";
+import EmojiDiagonal from "../emojis/emoji-diagonal";
 
 function ContentMapping() {
   const { tab } = useSelector((state: RootState) => state.holding);
@@ -12,6 +13,7 @@ function ContentMapping() {
     <AnimatePresence mode="wait">
       {tab === HOLDING_TABS.welcome && <Welcome key={1} />}
       {tab === HOLDING_TABS.zimojilogo && <Content key={2} />}
+      <EmojiDiagonal/>
     </AnimatePresence>
   );
 }
--- src/components/preloader/preloader.tsx
+++ src/components/preloader/preloader.tsx
@@ -54,7 +54,7 @@ export default function Preloader() {
         
         <ContentMapping  />
         <Footer />
-        <EmojiDiagonal/>
+        
       </div>
       </>
  
